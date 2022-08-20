a54dca58b53dd29f406a61f5a7be537f7a3c00a5
Biki Karmakar <biki@biki-system.biki.com>
2022-08-20 17:05:36 +0530

    update cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent,
-    FooterComponent
-  ],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatBadgeModule,
+    MatIconModule,
+    CarouselModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
+  products = [
+    {
+      url: 'https://www.kiranamarket.com/pub/media/wysiwyg/Grocery-Items-Cat.jpg',
+      name: 'Grocery',
+      pra: 'PRODUCTS',
+    },
+    {
+      url: 'https://www.kiranamarket.com/pub/media/wysiwyg/Health-Personal-Care-Items.jpg',
+      name: 'Health & Personal Care',
+      pra: 'PRODUCTS',
+    },
+    {
+      url: 'https://www.kiranamarket.com/pub/media/wysiwyg/Grocery-Items-Cat.jpg',
+      name: 'Grocery',
+      pra: 'PRODUCTS',
+    },
+    {
+      url: 'https://www.kiranamarket.com/pub/media/wysiwyg/Grocery-Items-Cat.jpg',
+      name: 'Grocery',
+      pra: 'PRODUCTS',
+    },
+    {
+      url: 'https://www.kiranamarket.com/pub/media/wysiwyg/Grocery-Items-Cat.jpg',
+      name: 'Grocery',
+      pra: 'PRODUCTS',
+    },
+    {
+      url: 'https://www.kiranamarket.com/pub/media/wysiwyg/Grocery-Items-Cat.jpg',
+      name: 'Grocery',
+      pra: 'PRODUCTS',
+    },
+    {
+      url: 'https://www.kiranamarket.com/pub/media/wysiwyg/Grocery-Items-Cat.jpg',
+      name: 'Grocery',
+      pra: 'PRODUCTS',
+    },
+    {
+      url: 'https://www.kiranamarket.com/pub/media/wysiwyg/Grocery-Items-Cat.jpg',
+      name: 'Grocery',
+      pra: 'PRODUCTS',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
 }
